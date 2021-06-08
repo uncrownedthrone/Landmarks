341b9f889a478c9899b669519b354a55179072a9
Justin Swiger <justin.swiger@me.com>
2021-06-08 10:45:08 -0400

    completed handling user input

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,13 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by Justin Swiger on 6/7/21.
-//
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
+	@StateObject private var modelData = ModelData()
+	
     var body: some Scene {
         WindowGroup {
             ContentView()
+				.environmentObject(modelData)
         }
     }
 }
--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -1,21 +1,14 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Justin Swiger on 6/7/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LandmarkList()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+			.environmentObject(ModelData())
     }
 }
